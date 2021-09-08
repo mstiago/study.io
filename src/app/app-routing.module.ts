275565122785e332ea0dcf10e34c81fed39a360d
Tiago Matos <tiagos1401@gmail.com>
2021-09-08 20:48:09 -0300

    Adding temporary home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './@theme/components/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'pages/home',
+  },
+  {
+    path: '',
+    children: [
+      { path: 'pages', children: [{ path: 'home', component: HomePageComponent }] },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
